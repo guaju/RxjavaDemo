61809598ea52f799b10c1c600925aa236a674486
guaju <guaju@126.com>
2018-07-19 11:06:36 +0800

    注释

--- app/src/main/java/com/guaju/rxjavademo/MainActivity.java
+++ app/src/main/java/com/guaju/rxjavademo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.guaju.rxjavademo;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ import java.net.URL;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -24,12 +28,42 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageView, imageView2;
     int count = 0;
 
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler =new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            int what=msg.what;
+            if (what==0){
+                String obj = (String) msg.obj;
+
+            }
+
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.iv);
         imageView2 = (ImageView) findViewById(R.id.iv2);
+
+
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                //处理数据
+                String  str="";
+                Message obtain = Message.obtain();
+                obtain.what=0;
+                obtain.obj=str;
+                mHandler.sendMessage(obtain);
+
+            }
+        });
 
 //        RxJava 1.0的版本：
 //        Observable  被观察者
@@ -142,6 +176,14 @@ public class MainActivity extends AppCompatActivity {
 
         //创建订阅者
         Subscriber subscriber = new Subscriber<BitmapDrawable>() {
+
+            @Override
+            public void onStart() {
+                System.out.println("我要开始啦~~~");
+
+
+            }
+
             @Override
             public void onCompleted() {
             }
@@ -160,9 +202,59 @@ public class MainActivity extends AppCompatActivity {
                 count++;
             }
         };
-        observable
-                .subscribeOn(Schedulers.io())// OnSubscribe所在的线程
+        Subscription subscribe = observable
                 .observeOn(AndroidSchedulers.mainThread()) //控制的是订阅者更新时所在线程
+                .subscribeOn(Schedulers.io())// OnSubscribe所在的线程
                 .subscribe(subscriber);
+
     }
+
+//    高级问题？
+//    subscribeOn
+//    observeOn
+//            分别能够调用几次？
+//
+//                subscribeOn方法只能调用1次
+//                 observeOn方法会调用多次
+//
+//
+//
+//
+//
+//    //Observable  被观察者
+////    create方法 ：创建Observable对象 ，并且 将OnSubscribe变量指定 call（）中有操作订阅者subscriber  onNext的逻辑
+////    subscribe方法：是先将订阅者Subscriber转成SafeSubscriber 然后 去调用  OnSubscribe的call方法 去申请"通知"
+//
+//
+//    //Observer    观察者
+////        接口：里面有三个方法
+////                onNext    更新
+////                onCompleted     更新结束
+////                onError         更新失败时
+//
+//
+//    //Subscriber  订阅者
+//       1、实现了Observer接口和Subscription接口 但是只实现了  Subscription接口的方法
+//            Observer接口的方法没有实现
+//       2、提供了onstart方法 这个方法会先于onNext方法执行，所以如果有初始化的操作，可以重写onStart中去使用
+//       3、内部有一个Subscriptionlist这样的类，他是用来记录所有订阅者和被观察着关系的类
+//
+//
+//    //OnSubscribe 被观察者 通知 观察者时做的操作  类似于观察者模式中的notify方法  能够管理通知的操作
+//      是一个接口，继承Action1的方法 里面有call()方法
+//
+//
+//
+//    //Subscription  描述观察者和被观察者关系的一个类
+////            接口：俩方法
+////                unSubscribe 取消订阅
+////                isUnSubscribed   判断是否取消订阅
+//
+//
+//
+//
+//
+//
+//
+
 }
